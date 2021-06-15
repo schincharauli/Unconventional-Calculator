0e77a339a53ba0cb5ff3749ff75719e01b327d97
schincharauli <52008243+schincharauli@users.noreply.github.com>
2021-06-15 21:15:28 +0400

    using shortcuts for operators

--- scripts/app.js
+++ scripts/app.js
@@ -16,7 +16,7 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
 function add() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
 
 }
@@ -24,21 +24,21 @@ function add() {
 function subtract(){
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -=  enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 function multiply(){
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *=  enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
 function divide(){
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /=  enteredNumber;
   createAndWriteOutput('/', initialResult, enteredNumber);
 
 }
